490f98fb32f31c3420672e4522bf5aeed32475d3
Daniel Seguin <63018494+danieloseguin67@users.noreply.github.com>
2025-11-06 01:38:59 -0500

    fix broken link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { NgClass } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   template: `
   <main>
+    <!-- Navigation bar -->
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 fixed-top">
+      <div class="container">
+        <a routerLink="/" class="navbar-brand">Daniel Seguin Organist Player</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu" id="menu button" aria-label="menu button">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navmenu">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item"><a routerLink="/" class="nav-link" routerLinkActive="active" [routerLinkActiveOptions]="{exact: true}">Home</a></li>
+            <li class="nav-item"><a routerLink="/about" class="nav-link" routerLinkActive="active">About the organist</a></li>
+            <li class="nav-item"><a routerLink="/specialrequest" class="nav-link" routerLinkActive="active">Make a Special Request</a></li>
+            <li class="nav-item"><a routerLink="/contactme" class="nav-link" routerLinkActive="active">Contact the Web Master</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
     <section class="content">
       <router-outlet></router-outlet>
     </section>
@@ -16,7 +32,6 @@ import { NgClass } from '@angular/common';
   `,
   styleUrls: ['./app.component.css'],
   imports: [
-    HomeComponent,
     RouterModule,
     FormsModule]
 })
